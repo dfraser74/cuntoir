46261b8b2f62d86348c8ae2f191be8562a5937c2
Oliver <oliverbrowne627@gmail.com>
2017-08-22 16:28:28 +0100

    Created a database template, continued work on setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import os
-from OpenSSL import ssl
+import OpenSSL
 
 def createConfigs():
     #create dbHosts.conf
     with open("dbHosts.conf", "w") as hostsFile:
         dbHosts = []
         host = raw_input("Enter the primary database hostname (e.g. localhost), or nothing to only use localhost: ")
-        while(dbHost):
+        while(host):
             dbHosts.append(host)
             host = raw_input("Enter another fallback host, or nothing to continue: ")
         if(not dbHosts):
@@ -17,7 +17,7 @@ def createConfigs():
     with open("invite.conf", "w") as inviteFile:
         print("Please note, right now invite codes aren't fully supported,")
         print("so if you use them you'll need to use the custom requests code given in the readme file to set up users, sorry.")
-        invite = raw_input("Enter theinvite code you wish to use, or nothing to leave signup open (recommended): ")
+        invite = raw_input("Enter the invite code you wish to use, or nothing to leave signup open (recommended): ")
         if(invite):
             inviteFile.write(invite+"\n")
     #create pass.conf
@@ -35,25 +35,31 @@ def createSSLKeys():
     if(not os.path.exists("ssl")):
         os.makedirs("ssl")
     with open("ssl/cert.pem", "w") as certFile:
-        certFileLoc = raw_input("Enter the location of your ssl 'fullchain.pem' file, or nothing to generate one: ")
+        certFileLoc = raw_input("Enter the location of your ssl 'fullchain.pem' file, or nothing to generate one (future feature, on the todo list): ")
         if(certFileLoc):
-            #TODO copy the certfile to ssl/cert.pem and ssl./chain.pem
+            with open(certFileLoc, "r") as oldCertFile:
+                certFile.write(oldCertFile.read())
+                with open("ssl/chain.pem", "w") as chainFile:
+                    chainFile.write(oldCertFile.read())
         else:
+            raise Exception("You need to specify an already generated ssl full chain file, sorry. It's on the todo list")
             #TODO code to generate an ssl cert file and ssl chain file
     with open("ssl/priv.key", "w") as keyFile:
         keyFileLoc = raw_input("Enter the location of your ssl 'privkey.pem' file, or nothing to generate one: ")
         if(keyFileLoc):
-            #TODO copy the private key file to ssl/priv.key
+             with open(keyFileLoc, "r") as oldKeyFile:
+                keyFile.write(oldKeyFile.read())
         else:
+            raise Exception("You need to specify an already generated ssl private key file, sorry. It's on the todo list")
             #TODO generate a key file - this will probably be done when the cert file  location is blank
 
 def createPushKeys():
     #NOTE this is extremely weird, I'll need to figure out a better way to do it, becuase right now the push public key is hard coded into
     #the script.js file. Hmmmmmmmmmm :/
     print("Time to generate some web push notification keys, wooooooo")
-    raw_input("visit https://web-push-codelab.appspot.com/ and generate a public and private key pair before continuing")
-    pubKey = raw_input("Enter the public key: ")
-    privKey = raw_input("Enter the private key: ")
+    raw_input("Please visit https://web-push-codelab.appspot.com/ and generate a public and private key pair before continuing")
+    pubKey = raw_input("Enter the public key: ").strip()
+    privKey = raw_input("Enter the private key: ").strip()
     if(not os.path.exists("pushKeys")):
         os.makedirs("pushKeys")
     with open("private_key.pem", "w") as privFile:
@@ -76,5 +82,5 @@ if __name__ == "__main__":
     createSSLKeys()
     createPushKeys()
     print("Everything should be set up now, config file wise.")
-    print("You still need to set up the database, and run 'sudo python server.py' and 'sudo python redirectServer.py', and then it should work.... Maybe")
+    print("You still need to set up the database, and run 'sudo python server.py' and 'sudo python redirectServer.py', and then it should work, hopefully.")
 
